f64383c8c978a4270638d8efb1b7bb267edcdfc8
bwc130428 <baewonchan1@naver.com>
2020-09-07 22:06:22 +0900

    Add debug_printf

--- lib/stdio.h
+++ lib/stdio.h
@@ -1,6 +1,24 @@
-#ifdef LIB_STDIO_H_
+/*
+ * stdio.h
+ *
+ *  Created on: Sep 17, 2018
+ *      Author: maanu
+ */
+
+#ifndef LIB_STDIO_H_
 #define LIB_STDIO_H_
 
-uint32_t putstr(const char* s);
+#include "stdarg.h"
 
-#endif /*LIBSTDIO_H_*/
+typedef enum utoa_t
+{
+    utoa_dec = 10,
+    utoa_hex = 16,
+} utoa_t;
+
+uint32_t putstr(const char* s);
+uint32_t debug_printf(const char* format, ...);
+uint32_t vsprintf(char* buf, const char* format, va_list arg);
+uint32_t utoa(char* buf, uint32_t val, utoa_t base);
+
+#endif /* LIB_STDIO_H_ */
