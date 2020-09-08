d1d4ab878a4978df2f2ee0b72a7b507383d7d84b
bwc130428 <baewonchan1@naver.com>
2020-09-08 23:31:45 +0900

    Add interrupt controller

--- boot/Main.c
+++ boot/Main.c
@@ -18,15 +18,8 @@ void main(void){
 	putstr("Hello World!\n");
 
 	Printf_test();
-
-	i = 100;
 	
-	while(i--)
-	{
-		uint8_t ch = Hal_uart_get_char();
-		Hal_uart_put_char(ch);
-	}
-
+	while(true);
 }
 
 static void Hw_init(void){
